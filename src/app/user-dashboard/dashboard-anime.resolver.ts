b1393989c9b334284b269fbf470b80195704f789
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-02-23 13:07:01 -0500

    Create resolver for popular anime on dashboard

---
+++ src/app/user-dashboard/dashboard-anime.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { KitsuService } from '../services/kitsu.service';
+import { Anime } from '../models/anime.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardAnimeResolver implements Resolve<Anime[]> {
+
+  public constructor(private kitsuService: KitsuService) { }
+
+  public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    return this.kitsuService.queryAnimePopular();
+  }
+}
