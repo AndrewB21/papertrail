fb0dc21488d5fcac6c66d54d3dbb9df2d9f8b387
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-02-23 16:26:48 -0500

    Add method to retrieve slugs of anime the user watching

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore, private afAuth: AngularFireAuth) { }
+
+  public getWatchingAnimeSlugs(): Observable<string[]> {
+    return this.afAuth.authState.pipe(switchMap((user) => {
+      if (user) {
+        return this.firestore.collection('UserAnime').doc(user.uid).collection('anime')
+        .get()
+        .pipe(
+          map((querySnapshot) => {
+            const animeSlugList: string[] = [];
+            querySnapshot.forEach((doc) => {
+                animeSlugList.push(doc.id);
+              });
+            return animeSlugList;
+          }));
+      } else {
+        return null;
+      }
+    }));
+  }
 }
