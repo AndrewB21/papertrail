2a9d97837d5fd72abf4eb738b2991d9bcfff8ff9
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-01 16:44:48 -0500

    Add reactive forms module, anime search component, and content browser component imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { ContentCardComponent } from './content-card/content-card.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { DashboardAuthenticationResolver } from './user-dashboard/resolvers/dashboard-authentication.resolver';
 import { PopularAnimeResolver } from './shared/resolvers/popular-anime.resolver';
-import { DashboardWatchingAnimeResolver } from './user-dashboard/resolvers/dashboard-watching-anime.resolver';
+import { AnimeSearchComponent } from './shared/components/anime-search/anime-search.component';
+import { ContentCardComponent } from './shared/components/content-card/content-card.component';
 import { ContentPageComponent } from './content-page/content-page.component';
-import { HomeComponent } from './home/home.component';
+import { ContentBrowserComponent } from './content-browser/content-browser.component';
 
 const routes: Routes = [
   {
@@ -22,7 +24,6 @@ const routes: Routes = [
     resolve: {
       authenticated: DashboardAuthenticationResolver,
       popularAnime: PopularAnimeResolver,
-      watchingNow: DashboardWatchingAnimeResolver,
     }
   },
   {
@@ -39,11 +40,14 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [RouterModule],
   declarations: [
     UserDashboardComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    AnimeSearchComponent,
+    ContentBrowserComponent,
   ],
 })
 export class AppRoutingModule { }
