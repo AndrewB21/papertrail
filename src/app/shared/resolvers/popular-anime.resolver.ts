381e08a50bc19e5f57ae0cd878d7088909883125
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-01 16:13:57 -0500

    Update method name

--- src/app/shared/resolvers/popular-anime.resolver.ts
+++ src/app/shared/resolvers/popular-anime.resolver.ts
@@ -15,7 +15,7 @@ export class PopularAnimeResolver implements Resolve<Anime[]> {
   public constructor(private kitsuService: KitsuService, private firestoreService: FirestoreService) { }
 
   public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-    return this.kitsuService.getPopularAnime().pipe(map((animeList: KitsuResponse) => {
+    return this.kitsuService.getPopularAnimeFromKitsu().pipe(map((animeList: KitsuResponse) => {
       return animeList.data;
     }));
   }
