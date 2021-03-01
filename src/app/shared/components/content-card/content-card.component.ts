f4ec057a1d00c07c92c280fc9a3e71fb24634d37
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-01 16:17:58 -0500

    Add comment to ngAfterViewChecked

--- src/app/shared/components/content-card/content-card.component.ts
+++ src/app/shared/components/content-card/content-card.component.ts
@@ -21,31 +21,31 @@ export class ContentCardComponent implements OnInit, AfterViewChecked {
   }
 
   public ngAfterViewChecked(): void {
+    // Initialize the anime's watching status if it has not already been initialized by a parent component.
+    // We use ngAfterViewChecked to wait for the watchingAnime array to be initialized propertly before
+    // performing the check and initialization
     if (this.watchingAnime !== undefined && this.anime.watching === undefined) {
       this.anime.watching = this.kitsuService.getWatchingAnime().some(element => element.id === this.anime.id);
       this.cdRef.detectChanges();
     }
   }
 
-
   public addAnimeToWatching() {
     this.anime.watching = true;
     this.firestoreService.addAnimeToWatching(this.anime.attributes.slug);
+    this.addToWatching.emit();
 
     // Update the watchingAnime list in KitsuService
     this.kitsuService.getWatchingAnime().push(this.anime);
-
-    this.addToWatching.emit();
   }
 
   public removeAnimeFromWatching() {
     this.anime.watching = false;
     this.firestoreService.removeAnimeFromWatching(this.anime.attributes.slug);
+    this.removeFromWatching.emit();
 
     // Update the watchingAnime list in KitsuService
     const kitsuServiceAnimeIndex = this.kitsuService.getWatchingAnime().findIndex(element => element.id === this.anime.id);
     this.kitsuService.getWatchingAnime().splice(kitsuServiceAnimeIndex, 1);
-
-    this.removeFromWatching.emit();
   }
 }
