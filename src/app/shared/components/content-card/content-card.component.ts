c213331e6320d55bbb2b25675be92d78092504a3
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-01 16:15:20 -0500

    Update add/remove methods for new firebase implementation

--- src/app/shared/components/content-card/content-card.component.ts
+++ src/app/shared/components/content-card/content-card.component.ts
@@ -1,31 +1,51 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewChecked, Component, EventEmitter, Input, OnInit, Output, ChangeDetectorRef } from '@angular/core';
 import { Anime } from 'src/app/models/anime.model';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { KitsuService } from 'src/app/services/kitsu.service';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.css']
 })
-export class ContentCardComponent implements OnInit {
+export class ContentCardComponent implements OnInit, AfterViewChecked {
   @Input() public anime: Anime;
   @Input() public index: number;
-  @Output() public addToWatching = new EventEmitter<number>();
-  @Output() public removeFromWatching = new EventEmitter<{popular: boolean, index: number, anime: Anime}>();
-  public constructor(private firestoreService: FirestoreService) { }
+  @Input() public watchingAnime: Anime[];
+  @Output() public addToWatching = new EventEmitter<null>();
+  @Output() public removeFromWatching = new EventEmitter<null>();
+
+  public constructor(private firestoreService: FirestoreService, private kitsuService: KitsuService, private cdRef: ChangeDetectorRef) { }
 
   public ngOnInit(): void {
   }
 
+  public ngAfterViewChecked(): void {
+    if (this.watchingAnime !== undefined && this.anime.watching === undefined) {
+      this.anime.watching = this.kitsuService.getWatchingAnime().some(element => element.id === this.anime.id);
+      this.cdRef.detectChanges();
+    }
+  }
+
+
   public addAnimeToWatching() {
     this.anime.watching = true;
     this.firestoreService.addAnimeToWatching(this.anime.attributes.slug);
-    this.addToWatching.emit(this.index);
+
+    // Update the watchingAnime list in KitsuService
+    this.kitsuService.getWatchingAnime().push(this.anime);
+
+    this.addToWatching.emit();
   }
 
   public removeAnimeFromWatching() {
     this.anime.watching = false;
     this.firestoreService.removeAnimeFromWatching(this.anime.attributes.slug);
-    this.removeFromWatching.emit({popular: this.anime.attributes.popularityRank <= 10, index: this.index, anime: this.anime});
+
+    // Update the watchingAnime list in KitsuService
+    const kitsuServiceAnimeIndex = this.kitsuService.getWatchingAnime().findIndex(element => element.id === this.anime.id);
+    this.kitsuService.getWatchingAnime().splice(kitsuServiceAnimeIndex, 1);
+
+    this.removeFromWatching.emit();
   }
 }
