5c9c7aeb50c093bf88ac29e72e77c59621b874b4
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-03 15:19:32 -0500

    Implement real time search using typeahead

--- src/app/shared/components/anime-search/anime-search.component.ts
+++ src/app/shared/components/anime-search/anime-search.component.ts
@@ -1,34 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Anime } from 'src/app/models/anime.model';
 import { KitsuService } from 'src/app/services/kitsu.service';
+import { fromEvent, Observable, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-anime-search',
   templateUrl: './anime-search.component.html',
   styleUrls: ['./anime-search.component.css']
 })
-export class AnimeSearchComponent implements OnInit {
-  public animeSearch: FormGroup;
+export class AnimeSearchComponent implements OnInit, AfterViewInit {
   public searchResults: Anime[];
-  @Input() public watchingAnime: Anime[];
+  public containerClass = 'search-container collapsed';
+  @ViewChild('input') public input: ElementRef;
 
-  public constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private kitsuService: KitsuService) { }
+
+  public constructor(
+    private route: ActivatedRoute,
+    private kitsuService: KitsuService) { }
 
   public ngOnInit(): void {
-    this.initializeForm();
   }
 
-  public initializeForm(): void {
-    this.animeSearch = this.formBuilder.group({
-      searchText: '',
-    });
-  }
+  public ngAfterViewInit() {
+    // Use typeahead for realtime results as the user inputs their search
+    const searchText: Observable<string> =
+      fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+          map(event => event.target.value),
+          startWith(''),
+          debounceTime(700),
+          distinctUntilChanged()
+      );
 
-  public onSubmit(): void {
-    this.kitsuService.searchForAnimeByText(this.animeSearch.value.searchText).subscribe((animeList: Anime[]) => {
-      this.searchResults = animeList;
-    });
+    searchText.pipe(
+      switchMap(search => {
+        // Avoid passing along empty search strings to reduce API calls and allow for the container to
+        // expand and collapse appropriately
+        if (search !== '') {
+          this.containerClass = 'search-container expanded';
+          return this.kitsuService.searchForAnimeByText(search);
+
+        } else {
+          this.containerClass = 'search-container collapsed';
+          return of(null);
+        }
+      })
+    ).subscribe((result) => this.searchResults = result);
   }
 }
